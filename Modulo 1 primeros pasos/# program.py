540e729fe4036093e3e16ab27a04fbc179527ad7
RuthHernandez21 <99503473+RuthHernandez21@users.noreply.github.com>
2022-02-16 15:12:13 -0600

    add: files cloud gui

---
+++ Modulo 1 primeros pasos/# program.py
@@ -0,0 +1,12 @@
+ # program.py
+
+ #Fecha
+from datetime import date
+print("Today's date is: " +str(date.today()))
+
+#Conversion parsecs#
+parsec = 11
+
+lightyears =  3.26156 *parsec
+
+print(str(parsec) + " parsec, is " + str(lightyears) + " lightyears")
